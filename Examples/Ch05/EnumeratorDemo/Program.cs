546a5ed0cb06d286f73dce5d1b909a9ff8c9275a
Michael Tsai <huanlin.tsai@gmail.com>
2015-03-25 16:57:21 +0800

    Added .gitignore file.

--- Examples/Ch05/EnumeratorDemo/Program.cs
+++ Examples/Ch05/EnumeratorDemo/Program.cs
@@ -26,7 +26,7 @@ namespace EnumeratorDemo
 
             while (enumrt.MoveNext())   // 透過列舉器來巡訪集合中的每一個元素
             {
-                string s = (string)enumrt.Current; // 取得目前的元素
+                string s = (string)enumrt.Current; // 取得目前的元素（取出後須手動轉型）
                 Console.WriteLine(s);
             }
         }
@@ -35,13 +35,13 @@ namespace EnumeratorDemo
         {
             Console.WriteLine("===== 泛型版本 =====");
 
-            var fruits = new List<string>() { "蘋果", "香蕉", "鳳梨" };
+            string[] fruits = { "蘋果", "香蕉", "鳳梨" };
             
-            IEnumerator<string> enumrt = fruits.GetEnumerator();    // 從集合物件取得列舉器
+            IEnumerator<string> enumrt = ((IEnumerable<string>)fruits).GetEnumerator();    // 從集合物件取得列舉器
 
             while (enumrt.MoveNext())   // 透過列舉器來巡訪集合中的每一個元素
             {
-                string s = enumrt.Current; // 取得目前的元素
+                string s = enumrt.Current; // 取得目前的元素（不需要手動轉型）
                 Console.WriteLine(s);
             }
         }
