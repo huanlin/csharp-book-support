961a1fa35484625cef365ece5389a43d00473795
Michael Tsai <huanlin.tsai@gmail.com>
2016-07-14 01:03:10 +0800

    Added LINQ examples.

---
+++ Examples/Ch06/Demo07_Counting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using CommonClasses;
+
+namespace Demo07_Counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = CountEmployeesWithHighSalary();
+            int total = GetEmployeeSalaryTotal();
+        }
+
+        static int CountEmployeesWithHighSalary()
+        {
+            Employee[] employees = Employee.GetEmployees();
+
+            /* 未使用 LINQ 的寫法
+            int count = 0;
+            foreach (var emp in employees)
+            {
+                if (emp.Salary >= 60000)
+                {
+                    count++;
+                }
+            }
+            return count;
+            */
+
+            // 使用 LINQ
+            return employees.Count(e => e.Salary >= 60000);
+        }
+
+        static int GetEmployeeSalaryTotal()
+        {
+            Employee[] employees = Employee.GetEmployees();
+
+            /* 未使用 LINQ 的寫法
+            int salaryTotal = 0;
+            foreach (var emp in employees)
+            {
+                salaryTotal += emp.Salary;
+            }
+            return salaryTotal;
+            */
+
+            // 使用 LINQ
+            return employees.Sum(e => e.Salary);
+        }
+
+    }
+}
