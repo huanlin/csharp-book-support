47bb8be7e00e5607d70c096909eb038ec474c9ee
Michael Tsai <huanlin.tsai@gmail.com>
2016-07-16 09:31:08 +0800

    Minor change.

--- Examples/Ch06/Demo03_Filter/Program.cs
+++ Examples/Ch06/Demo03_Filter/Program.cs
@@ -13,7 +13,13 @@ namespace Demo03_Filter
             foreach(var emp in employees.Where(e => e.Department == "Sales"))
             {
                 System.Console.WriteLine(emp.Name);
-            }            
+            }
+
+            System.Console.WriteLine("=== 使用 Distinct() 來移除重複的元素 ===");
+            foreach (var department in employees.Select(e => e.Department).Distinct())
+            {
+                System.Console.WriteLine(department);
+            }
         }
 
 
