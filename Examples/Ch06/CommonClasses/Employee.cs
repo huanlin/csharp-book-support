8353a3107cb4eb9486e5318ec68bf1f31d23e638
Michael Tsai <huanlin.tsai@gmail.com>
2016-07-16 20:30:53 +0800

    Minor

--- Examples/Ch06/CommonClasses/Employee.cs
+++ Examples/Ch06/CommonClasses/Employee.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-namespace CommonClasses
+﻿namespace CommonClasses
 {
     public class Employee
     {
+        public int ID;
         public string Name;
-        public DateTime Birthday;
+        public string City;
         public string Department;
         public int Salary;
 
@@ -13,11 +12,12 @@ namespace CommonClasses
         {
             return new Employee[]
             {
-                new Employee { Name = "Michael", Department = "RD",    Salary = 59000 },
-                new Employee { Name = "Ben",     Department = "RD",    Salary = 73000 },
-                new Employee { Name = "Vivid",   Department = "Sales", Salary = 26500 },
-                new Employee { Name = "John",    Department = "Sales", Salary = 26500 },
-                new Employee { Name = "Sam",     Department = "HR",    Salary = 88000 }
+                new Employee { ID=1, Name="Mike",  City="台北", Department="RD",    Salary=59000 },
+                new Employee { ID=2, Name="Ben",   City="紐約", Department="RD",    Salary=73000 },
+                new Employee { ID=3, Name="Vivid", City="紐約", Department="Sales", Salary=26500 },
+                new Employee { ID=4, Name="John",  City="台北", Department="Sales", Salary=26500 },
+                new Employee { ID=5, Name="Sam",   City="紐約", Department="HR",    Salary=88000 },
+                new Employee { ID=6, Name="Ken",   City="紐約", Department="IT",    Salary=66000 }
             };
         }
     }
