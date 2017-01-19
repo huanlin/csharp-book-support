45cf110cfe5562a96ffdab56f6457398c63bea7f
Michael Tsai <huanlin.tsai@gmail.com>
2017-01-20 02:25:08 +0800

    Added Aggregate demo.

--- Examples/Ch06/Demo06_BooleanOperations/Program.cs
+++ Examples/Ch06/Demo06_BooleanOperations/Program.cs
@@ -11,7 +11,7 @@ namespace Demo06_BooleanOperations
             Employee[] employees = Employee.GetEmployees();
 
             bool isJohnHere = employees.Any(e => e.Name == "John"); // 傳回 true
-            bool isSalaryLeagal = employees.All(e => e.Salary > 20008); // 傳回 true
+            bool isSalaryLeagal = employees.All(e => e.Salary > 20000); // 傳回 true
 
             Console.WriteLine(isJohnHere);
             Console.WriteLine(isSalaryLeagal);
--- Examples/Ch06/Demo08_Counting/Program.cs
+++ Examples/Ch06/Demo08_Counting/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using CommonClasses;
 
 namespace Demo08_Counting
@@ -9,6 +10,10 @@ namespace Demo08_Counting
         {
             int count = CountEmployeesWithHighSalary();
             int total = GetEmployeeSalaryTotal();
+
+            Console.WriteLine("員工薪資總計：" + total);
+
+            DemoAggregate();
         }
 
         static int CountEmployeesWithHighSalary()
@@ -46,6 +51,21 @@ namespace Demo08_Counting
 
             // 使用 LINQ
             return employees.Sum(e => e.Salary);
+        }
+
+        // 使用 Aggregate() 來達成 Sum() 的效果（純粹為了方便理解而寫的範例，不是建議你這樣用）。
+        static void DemoAggregate()
+        {
+            Console.Write("Aggregate() 範例的輸出結果：");
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int result = numbers.Aggregate((current, next) => current + next);
+            // 以下註解說明了上一行程式碼的累計函式的內部運作過程：
+            // 1 + 2 = 3
+            // 3 + 3 = 6
+            // 6 + 4 = 10
+            // 10 + 5 = 15
+            Console.WriteLine(result); // 輸出 15。
         }
 
     }
