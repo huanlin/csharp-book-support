c2df03521764f4a37617ec315b422b8a1fca8ade
Michael Tsai <huanlin.tsai@gmail.com>
2026-02-08 22:00:02 +0800

    -

--- example-code-zh-tw/ch12/DemoSpan/Program.cs
+++ example-code-zh-tw/ch12/DemoSpan/Program.cs
@@ -11,13 +11,13 @@ Console.WriteLine(new string('-', 40));
 
 int[] array = { 1, 2, 3, 4, 5 };
 Span<int> span = array.AsSpan();
-Span<int> subSpan = span.Slice(1, 3); // 指向 [2, 3, 4]
+Span<int> slice = span.Slice(1, 3); // 指向 [2, 3, 4]
 
 Console.WriteLine($"原始陣列[1]: {array[1]}"); // 2
 
 // 修改 Span 會直接影響原陣列
-subSpan[0] = 99;
-Console.WriteLine($"修改 SubSpan[0] 為 99 後，原始陣列[1]: {array[1]}"); // 99
+slice[0] = 99;
+Console.WriteLine($"修改 slice[0] 為 99 後，原始陣列[1]: {array[1]}"); // 99
 
 
 // --------------------------------------------------------------
