941a2064040c06666c7954f4d5dea2d6559f39ee
Michael Tsai <huanlin.tsai@gmail.com>
2026-03-23 22:12:30 +0800

    ch08

--- example-code-en/ch08/DemoDelegateMulticast/Program.cs
+++ example-code-en/ch08/DemoDelegateMulticast/Program.cs
@@ -5,31 +5,20 @@ Console.WriteLine(new string('-', 40));
 
 Action? notifyAll = null;
 
+Action notifyAdmin = () => Console.WriteLine("  Notify Administrator");
+Action writeLog = () => Console.WriteLine("  Record Log");
+Action sendMail = () => Console.WriteLine("  Send Email");
+
 // Use += to add methods
-notifyAll += () => Console.WriteLine("  Notify Administrator");
-notifyAll += () => Console.WriteLine("  Record Log");
-notifyAll += () => Console.WriteLine("  Send Email");
+notifyAll += notifyAdmin;
+notifyAll += writeLog;
+notifyAll += sendMail;
 
 Console.WriteLine("Executing all notifications:");
 notifyAll?.Invoke();
 
-// Remove one of the methods
-Console.WriteLine("\nAfter removing 'Record Log':");
-// Note: You cannot directly remove it here because the lambda is a new instance of an anonymous method.
-// The example above is mainly to demonstrate the += syntax.
-// To correctly remove it, you usually need a named method or to store the delegate instance.
-
-// Example of correct removal:
-Action logAction = () => Console.WriteLine("  [Named] Record Log");
-notifyAll = null;
-notifyAll += () => Console.WriteLine("  [Named] Notify Administrator");
-notifyAll += logAction;
-
-Console.WriteLine("Resetting and adding named delegate...");
-notifyAll?.Invoke();
-
-Console.WriteLine("\nAfter removing the named delegate:");
-notifyAll -= logAction;
+Console.WriteLine("\nAfter removing 'Send Email':");
+notifyAll -= sendMail;
 notifyAll?.Invoke();
 
 Console.ReadKey();
